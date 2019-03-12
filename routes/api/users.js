ff190fe1ce29b6744b4ed52250e47dcbb79137f3
Ashwini Bhagavan <ashwini.aki2@github.com>
2019-03-12 12:28:54 -0700

    Models and api

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,13 @@
 const express = require ('express') ;
 const router = express.Router();
-router.get ('/test', (req, res) => res.json ({msg:'user api works'}));
+
+// Load user model
+const User =require ('../../models/User');
+
+
+// @route  POST api/users/register
+// @desc   Register user
+// @access public 
+
+
 module.exports = router;
