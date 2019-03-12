a5475508a8e357ac8e4bb4fd6b7c46c9ffe59213
Ashwini Bhagavan <ashwini.aki2@github.com>
2019-03-12 11:53:47 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require ('express');
+const mongoose = require ('mongoose');
+const app = express ();
+
+const db = require('./config/keys').mongoURI;
+const users =require ('./routes/api/users');
+const posts =require ('./routes/api/posts');
+const profile =require ('./routes/api/profile');
+
+//Connect to db
+mongoose
+.connect (db)
+.then (() => console.log('mongodb connected'))
+.catch (err => console.log(err));
+
+//routing
+app.get('/', (req, res)=> res.send ('Hello!'));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile );
+app.use('/api/posts', posts );
+
+const port = process.env.PORT || 6544;
+app.listen(port, ()=> console.log(`Server Running ${port}`));
+
+
